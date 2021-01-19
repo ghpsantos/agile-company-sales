57c15870ff59d0ac6384eab2b358b15e4df0c0e3
ghpsantos <ghps@cin.ufpe.br>
2021-01-19 00:15:21 -0300

    api sync deletions

--- resources/routes.py
+++
@@ -1,7 +0,0 @@
-from resources.user import UserApi, TopFiveApi,UserRevenueApi, UserSalesApi
-
-def initialize_routes(api):
-    api.add_resource(UserApi, '/api/users')
-    api.add_resource(TopFiveApi, '/api/users/top')
-    api.add_resource(UserRevenueApi, '/api/users/revenue')
-    api.add_resource(UserSalesApi, '/api/users/sales/<int:id>')
